983788715c8c8de20d4fbe8b3d444f28a6efbed4
Chris Rose <crose@hcsdoh.org>
2016-05-06 12:04:56 -0400

    Updated

---
+++ core/abre_functions.php
@@ -0,0 +1,136 @@
+<?php
+	
+	/*
+	* Copyright 2015 Hamilton City School District	
+	* 		
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+	* 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+	* 
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+	
+	//Required configuration files
+	require_once(dirname(__FILE__) . '/../configuration.php'); 
+	
+	//Encryption function
+	function encrypt($string, $encryption_key){
+		$encryption_key=constant("DB_KEY");
+		$string = rtrim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $encryption_key, $string, MCRYPT_MODE_ECB)));
+		return $string;
+	}
+	
+	//Decryption function
+	function decrypt($string, $encryption_key){
+		$encryption_key=constant("DB_KEY");
+		$string = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $encryption_key, base64_decode($string), MCRYPT_MODE_ECB));
+		return $string;
+	}
+	
+	//Find user ID in directory module given an email
+	function finduserid($email){
+		$email=encrypt($email, "");
+		$sql = "SELECT *  FROM directory where email='$email'";
+		$result = $db->query($sql);
+		while($row = $result->fetch_assoc())
+		{
+			$id=$row["id"];
+			return $id;
+		}
+	}
+	
+	//Find user ID given an email
+	function finduseridcore($email){
+		include "abre_dbconnect.php";
+		$sql = "SELECT *  FROM users where email='".$_SESSION['useremail']."'";
+		$result = $db->query($sql);
+		while($row = $result->fetch_assoc())
+		{
+			$id=$row["id"];
+			return $id;
+		}
+	}
+	
+	//Determine the grades that students do not have email access
+	function studentaccess(){
+		$email = $_SESSION['useremail'];
+		if(preg_replace('/[^0-9]+/', '', $email))
+		{
+			$gradyear = intval(preg_replace('/[^0-9]+/', '', $email), 10);
+			$currentyear = date("y");
+			$difference=$gradyear-$currentyear;
+			if($difference<6){
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	//Query the database
+	function databasequery($query){
+		include "abre_dbconnect.php";
+		$result = $db->query($query);
+		$rowarray = array();
+		while($row = $result->fetch_assoc())
+		{
+			array_push($rowarray, $row);	
+		}
+		return $rowarray;
+		$db->close();
+	}
+	
+	//Insert into the database
+	function databaseexecute($query){
+		include "abre_dbconnect.php";
+		$stmt = $db->stmt_init();
+		$stmt->prepare($query);
+		$stmt->execute();
+		$newcommentid = $stmt->insert_id;
+		$stmt->close();
+		return $newcommentid;
+		$db->close();
+	}
+	
+	function linkify($value, $protocols = array('http', 'mail'), array $attributes = array())
+    {
+        // Link attributes
+        $attr = '';
+        foreach ($attributes as $key => $val) {
+            $attr = ' ' . $key . '="' . htmlentities($val) . '"';
+        }
+        
+        $links = array();
+        
+        // Extract existing links and tags
+        $value = preg_replace_callback('~(<a .*?>.*?</a>|<.*?>)~i', function ($match) use (&$links) { return '<' . array_push($links, $match[1]) . '>'; }, $value);
+        
+        // Extract text links for each protocol
+        foreach ((array)$protocols as $protocol) {
+            switch ($protocol) {
+                case 'http':
+                case 'https':   $value = preg_replace_callback('~(?:(https?)://([^\s<]+)|(www\.[^\s<]+?\.[^\s<]+))(?<![\.,:])~i', function ($match) use ($protocol, &$links, $attr) { if ($match[1]) $protocol = $match[1]; $link = $match[2] ?: $match[3]; return '<' . array_push($links, "<a $attr href=\"$protocol://$link\" target=\"_blank\" class='mdl-color-text--blue-800'>$link</a>") . '>'; }, $value); break;
+                case 'mail':    $value = preg_replace_callback('~([^\s<]+?@[^\s<]+?\.[^\s<]+)(?<![\.,:])~', function ($match) use (&$links, $attr) { return '<' . array_push($links, "<a $attr href=\"mailto:{$match[1]}\" target=\"_blank\" class='mdl-color-text--blue-800'>{$match[1]}</a>") . '>'; }, $value); break;
+                case 'twitter': $value = preg_replace_callback('~(?<!\w)[@#](\w++)~', function ($match) use (&$links, $attr) { return '<' . array_push($links, "<a $attr href=\"https://twitter.com/" . ($match[0][0] == '@' ? '' : 'search/%23') . $match[1]  . "\" target=\"_blank\" class='mdl-color-text--blue-800'>{$match[0]}</a>") . '>'; }, $value); break;
+                default:        $value = preg_replace_callback('~' . preg_quote($protocol, '~') . '://([^\s<]+?)(?<![\.,:])~i', function ($match) use ($protocol, &$links, $attr) { return '<' . array_push($links, "<a $attr href=\"$protocol://{$match[1]}\" target=\"_blank\" class='mdl-color-text--blue-800'>{$match[1]}</a>") . '>'; }, $value); break;
+            }
+        }
+        
+        // Insert all link
+        return preg_replace_callback('/<(\d+)>/', function ($match) use (&$links) { return $links[$match[1] - 1]; }, $value);
+    }
+    	
+?>
