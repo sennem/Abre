a32fa5e8b2af2f9ed3cd68b9915c0ba972e3ac00
Chris Rose <crose@hcsdoh.org>
2016-05-11 08:43:02 -0400

    Fix stream comment table add

--- modules/stream/config.php
+++ modules/stream/config.php
@@ -26,7 +26,6 @@
 	if(!$resultstreams = $db->query("SELECT *  FROM streams"))
 	{
 		$sql = "CREATE TABLE `streams` (`id` int(11) NOT NULL,`group` text NOT NULL,`title` text NOT NULL,`slug` text NOT NULL,`type` text NOT NULL,`url` text NOT NULL,`required` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-		$sql .= "CREATE TABLE `streams_comments` (`id` int(11) NOT NULL,`url` text NOT NULL,`user` text NOT NULL,`comment` text NOT NULL,`creationtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `liked` int(11) NOT NULL DEFAULT '0') ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 		$sql .= "ALTER TABLE `streams` ADD PRIMARY KEY (`id`);";
   		$sql .= "ALTER TABLE `streams` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
   		$sql .= "INSERT INTO `streams` (`id`, `group`, `title`, `slug`, `type`, `url`, `required`) VALUES (NULL, 'staff', 'Resident Educator', 'residenteducator', 'flipboard', 'https://flipboard.com/@loripierson/hcsd-resident-educator-resources-ani2c718y.rss', '0');";
@@ -38,7 +37,7 @@
 	//Setup tables if new module
 	if(!$resultstreamscomments = $db->query("SELECT *  FROM streams_comments"))
 	{
-		$sql = "CREATE TABLE IF NOT EXISTS `streams_comments` (`id` int(11) NOT NULL AUTO_INCREMENT,`url` text NOT NULL,`title` text NOT NULL,`user` text NOT NULL,`comment` text NOT NULL,`creationtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,`liked` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1";
+		$sql = "CREATE TABLE `streams_comments` (`id` int(11) NOT NULL AUTO_INCREMENT,`url` text NOT NULL,`title` text NOT NULL,`user` text NOT NULL,`comment` text NOT NULL,`creationtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,`liked` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1";
   		if ($db->multi_query($sql) === TRUE) { }
 	}
 
