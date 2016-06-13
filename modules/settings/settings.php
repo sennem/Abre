b3d798e8d1e4a66d659330e399b36fea1fe05ff2
Chris Rose <crose@hcsdoh.org>
2016-06-13 09:42:03 -0400

    Update to pull latest release directly from GitHub

--- modules/settings/settings.php
+++ modules/settings/settings.php
@@ -100,22 +100,24 @@
 					
 					echo "<div class='row'>";
 						echo "<div class='col s12'><div class='col s12'>";
-							$updatejson = file_get_contents("http://updates.abre.io/");
-							if ($updatejson !== FALSE)
+								
+							//Retrieve latest version from public GitHub repo
+							$opts = ['http' => ['method' => 'GET','header' => ['User-Agent: PHP']]];
+							$context = stream_context_create($opts);
+							$content = file_get_contents("https://api.github.com/repos/abreio/Abre/releases/latest", false, $context);
+							$json = json_decode($content, true);
+							$currentversion = $json['name'];
+							$currentlink = $json['zipball_url'];
+								
+							if($abre_version<$currentversion)
 							{
-								$updatedata = json_decode($updatejson, true);
-								$currentversion = $updatedata['version'];
-								$currentlink = $updatedata['link'];
-								if($abre_version<$currentversion)
-								{
-									echo "<a id='updateabre' href='#' data-version='$currentlink' style='color:#999'>You have version $abre_version installed. Update to $currentversion.</a>";
-								}
-								else
-								{
-									echo "<p style='color:#999'>Release: $abre_version</p>";
-								}
-									
+								echo "<a id='updateabre' href='#' data-version='$currentlink' style='color:#999'>You have version $abre_version installed. Update to $currentversion.</a>";
 							}
+							else
+							{
+								echo "<p style='color:#999'>Release: $abre_version</p>";
+							}
+							
 						echo "</div></div>";
 					echo "</div>";
 					
@@ -133,7 +135,10 @@
 		event.preventDefault();
 		var Link = $(this).data('version');
 		$(this).html("Updating...");
-		$.post("modules/settings/update.php", { link: Link },function(){ location.reload(); });
+		$.post("modules/settings/update.php", { link: Link }, function(){ })
+		.done(function() {
+			location.reload();
+  		})
   	});
 	
 	//Provide image upload on icon click
