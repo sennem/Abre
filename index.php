6363d2ad4875219c257260bc5ac13a98444cb364
Christopher Rose <crose@hcsdoh.org>
2016-12-08 09:12:54 -0500

    Test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	/*
-	* Copyright 2015 Hamilton City School District	
+	* Copyright 2015 Hamilton City School District
 	* 		
 	* This program is free software: you can redistribute it and/or modify
     * it under the terms of the GNU General Public License as published by
