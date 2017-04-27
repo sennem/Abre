dbb85508f1d9b1be4121b58c152461ca043b0b94
Brandon Wilson <wilso199@miamioh.edu>
2017-04-27 13:56:55 -0400

    Added code to start testing facebook oauth

--- core/abre_parent_login.php
+++ core/abre_parent_login.php
@@ -20,7 +20,18 @@
 	//Include required files
 	require_once(dirname(__FILE__) . '/../configuration.php');
 	require_once('abre_functions.php');
+  require(dirname(__FILE__). '/facebook/src/Facebook/autoload.php');
 
+  $fb = new Facebook\Facebook([
+    'app_id' => '1437651372975104', // Replace {app-id} with your app id
+    'app_secret' => '9f1fcbb500aec4aca39d95ca672823df',
+    'default_graph_version' => 'v2.9',
+    ]);
+
+  $helper = $fb->getRedirectLoginHelper();
+
+  $permissions = ['public_profile']; // Optional permissions
+  $loginUrl = $helper->getLoginUrl('http://localhost/core/facebook_login_helper.php', $permissions);
 ?>
 
 	<!-- Display the login -->
@@ -30,15 +41,9 @@
 			<div class="login-card-square mdl-card animated fadeIn">
 				<div class="mdl-card__title mdl-card--expand" style='width:200px; height:200px; background: url(<?php echo sitesettings('sitelogo'); ?>) center center no-repeat; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; margin-left:20px; margin-bottom:5px;'></div>
 				<?php
-					echo "<div class='mdl-card-text mdl-color-text--grey-600'>".sitesettings('sitelogintext')."</div>";
-
-			  		echo "<a class='waves-effect waves-light btn-large mdl-color-text--white' href='$authUrl' style= 'margin: 0 auto; width:100%; text-transform:none; background-color:".sitesettings("sitecolor")."'>Staff and Student Login</a>";
-						if(sitesettings("parentaccess") == "checked"){
-							echo "<div style= 'padding-top: 2em'>";
-								echo "<a class='waves-effect waves-light btn-large mdl-color-text--white' href='$parent_root?usertype=Parent' style='margin: 0 auto; width:100%; text-transform:none; background-color:".sitesettings("sitecolor")."'>Parent Sign in</a>";
-							echo "</div>";
-						}
-					?>
+					echo "<div class='mdl-card-text mdl-color-text--grey-600'>Please log in with one of the following services</div>";
+          echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+			  	?>
 			</div>
 
 		</div>
@@ -63,4 +68,11 @@
 		loginWidthCheck();
 		$(window).resize(loginWidthCheck);
 
+    function fbLogin() {
+        var oauth_url = 'https://www.facebook.com/dialog/oauth/';
+        oauth_url += '?client_id=1437651372975104';
+        oauth_url += '&redirect_uri=' + encodeURIComponent('http://localhost/index.php');
+        oauth_url += '&scope=email'
+        window.top.location = oauth_url;
+    }
 	</script>
--- index.php
+++ index.php
@@ -1,53 +1,56 @@
-<?php 
-	
+<?php
+
 	/*
 	* Copyright 2015 Hamilton City School District
-	* 		
+	*
 	* This program is free software: you can redistribute it and/or modify
     * it under the terms of the GNU General Public License as published by
     * the Free Software Foundation, either version 3 of the License, or
     * (at your option) any later version.
-	* 
+	*
     * This program is distributed in the hope that it will be useful,
     * but WITHOUT ANY WARRANTY; without even the implied warranty of
     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     * GNU General Public License for more details.
-	* 
+	*
     * You should have received a copy of the GNU General Public License
     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
     */
-    
+
 	//Start PHP session if one does not exist
 	if(session_id() == ''){ session_start(); }
-	
+
 	//Check for configuration file and run installer if one does not exist
 	if (file_exists('configuration.php'))
 	{
-		
+
 		//Load configuration file, google authentication, and site header
 		require_once('configuration.php');
-		
+
 		//Look for redirected urls
 		if(isset($_GET["url"]))
-		{ 
-			$_SESSION["redirecturl"]=htmlspecialchars($_GET["url"]); 
+		{
+			$_SESSION["redirecturl"]=htmlspecialchars($_GET["url"]);
 		}
-		
+
 		//If there is a session redirect, load the redirect url
 		if(isset($_GET["url"]) && isset($_SESSION['useremail']))
-		{ 
-			$_SESSION["redirecturl"]=htmlspecialchars($_GET["url"]); 
+		{
+			$_SESSION["redirecturl"]=htmlspecialchars($_GET["url"]);
 			header("Location: $portal_root/#".$_SESSION["redirecturl"]);
 		}
-		
+
 		//Include Google Login
 		require_once('core/abre_google_login.php');
-		
+
 		//Include Site Header
 		require_once('core/abre_header.php');
-	
+
 		//Display login if user is not logged in
-		if(isset($authUrl))
+		if(isset($_GET["usertype"])){
+			require_once('core/abre_parent_login.php');
+		}
+		else if(isset($authUrl))
 		{
 			require_once('core/abre_login.php');
 		}
@@ -56,18 +59,17 @@
 			require_once('core/abre_load_modules.php');
 			require_once('core/abre_layout_page.php');
 		}
-		
+
 		//Display site close and footer
 		require_once('core/abre_footer.php');
-		
 	}
 	else
 	{
-		
+
 		//Display the installer
 		require('core/abre_installer.php');
 		require_once('core/abre_footer.php');
-		
+
 	}
-	
+
 ?>
