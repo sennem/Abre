a6724bdcdd9c8a1e7127872c1b683797b0d1f3de
Brandon Wilson <wilso199@miamioh.edu>
2017-05-07 18:47:52 -0400

    Added MIT license to top of file

---
+++ core/abre_facebook_login_helper.php
@@ -0,0 +1,149 @@
+<?php
+/*
+* Copyright 2015 Hamilton City School District
+*
+* This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+*
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+*
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+  if(session_id() == ''){ session_start(); }
+  require_once(dirname(__FILE__) . '/../configuration.php');
+  require_once('abre_functions.php');
+  require(dirname(__FILE__). '/facebook/src/Facebook/autoload.php');
+
+  $fb = new Facebook\Facebook([
+    'app_id' => sitesettings('facebookclientid'), // Replace {app-id} with your app id
+    'app_secret' => sitesettings('facebookclientsecret'),
+    'default_graph_version' => 'v2.9',
+  ]);
+
+  $helper = $fb->getRedirectLoginHelper();
+
+  try {
+    $accessToken = $helper->getAccessToken();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
+
+  if (!isset($accessToken)) {
+    if ($helper->getError()) {
+      header('HTTP/1.0 401 Unauthorized');
+      echo "Error: " . $helper->getError() . "\n";
+      echo "Error Code: " . $helper->getErrorCode() . "\n";
+      echo "Error Reason: " . $helper->getErrorReason() . "\n";
+      echo "Error Description: " . $helper->getErrorDescription() . "\n";
+    } else {
+      header('HTTP/1.0 400 Bad Request');
+      echo 'Bad request';
+    }
+    exit;
+  }
+
+  // Logged in
+  $_SESSION['facebook_access_token'] = $accessToken->getValue();
+  $pagelocation=$portal_root;
+  if(isset($_SESSION["redirecturl"])){ header("Location: $pagelocation/#".$_SESSION["redirecturl"]); }else{ header("Location: $pagelocation"); }
+
+  $response = $fb->get('/me?fields=name,email', $accessToken->getValue());
+  $user = $response->getGraphUser();
+  $userid = $user['id'];
+  $revokeCall = '/'. $userid .'/permissions';
+
+try{
+
+  // access token but useremail not set
+  if(isset($_SESSION['facebook_access_token']))
+  {
+    if(!isset($_SESSION['useremail']))
+    {
+      $_SESSION['useremail']=$user['email'];
+      $_SESSION['usertype']= 'parent';
+      $_SESSION['displayName']= $user['name'];
+      $_SESSION['picture'] = sitesettings('sitelogo');
+    }
+  }
+
+  if (isset($_SESSION['facebook_access_token']))
+  {
+    if($_SESSION['usertype']!="")
+    {
+      include "abre_dbconnect.php";
+      if($result=$db->query("SELECT * from users_parent where email='".$_SESSION['useremail']."'"))
+      {
+        $count=$result->num_rows;
+        if($count=='1')
+        {
+          //If not already logged in, check and get a refresh token
+          if (!isset($_SESSION['loggedin'])) { $_SESSION['loggedin']=""; }
+          if($_SESSION['loggedin']!="yes")
+          {
+            //Mark that they have logged in
+            $_SESSION['loggedin']="yes";
+          }
+        }
+        else
+        {
+          $sha1useremail=sha1($_SESSION['useremail']);
+          $storetoken=$sha1useremail.$hash;
+          mysqli_query($db, "Insert into users_parent (email, students) VALUES ('".$_SESSION['useremail']."', '')") or die (mysqli_error($db));
+        }
+
+      }
+      $db->close();
+    }
+
+  }
+}
+catch (Exception $x)
+{
+
+  if(strpos($x->getMessage(), 'Invalid Credentials'))
+  {
+
+
+    session_destroy();
+    $fb->api($revokeCall, "DELETE", $accessToken);
+    //$client->revokeToken();
+
+    //Redirect user
+    //header("Location: $portal_root");
+
+  }
+
+  if(strpos($x->getMessage(), 'Invalid Credentials'))
+  {
+
+    //Destroy the OAuth & PHP session
+    session_destroy();
+    $fb->api($revokeCall, "DELETE", $accessToken);
+    //$client->revokeToken();
+
+    //Redirect user
+    //header("Location: $portal_root");
+  }
+
+}
+
+  header("Location: $portal_root");
+  // User is logged in with a long-lived access token.
+  // You can redirect them to a members-only page.
+  //header('Location: https://example.com/members.php');
+
+
+ ?>
