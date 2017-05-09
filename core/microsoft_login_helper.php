9a2b429c2bf6f926ff95de2fb71e6d71a706e5f3
Brandon Wilson <wilso199@miamioh.edu>
2017-05-09 12:18:41 -0400

    Made changes to login helper to parse id token

--- core/microsoft_login_helper.php
+++ core/microsoft_login_helper.php
@@ -5,12 +5,12 @@
   require_once('abre_functions.php');
 
   $fields = array(
-  	'client_id' => urlencode('92c0fe4c-9956-4f78-940d-f1922b9f2fc2'),
+  	'client_id' => urlencode(sitesettings('microsoftclientid')),
   	'redirect_uri' => urlencode('http://localhost/core/microsoft_login_helper.php'),
   	'grant_type' => urlencode('authorization_code'),
-  	'client_secret' => urlencode('nAwj6mJDkMvc3ik6s6FRyPc'),
+  	'client_secret' => urlencode(sitesettings('microsoftclientsecret')),
   	'code' => urlencode($_POST['code']),
-  	'scope' => urlencode('openid')
+  	'scope' => urlencode('openid profile')
   );
 
   //url-ify the data for the POST
@@ -26,106 +26,93 @@
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $result = curl_exec($ch);
   $json = json_decode($result, true);
-  echo $result;
-  echo $json['id_token'];
+  $accessToken = $json['id_token'];
+  $payload = explode(".", $accessToken);
+  $info = $payload[1];
 
-  function base64_url_decode($input)
-  {
-      return base64_decode(strtr($input, '-_,', '+/='));
-  }
-  //Logged in
-  echo base64_url_decode($json['id_token']);
-   $_SESSION['microsoft_access_token'] = base64_decode($json['id_token'], true);
-   echo base64_decode($json['id_token'], true);
+   //Logged in
+   $infoString = base64_decode($info);
+   $infoObject = json_decode($infoString);
+   $_SESSION['microsoft_access_token'] = $accessToken;
    $pagelocation=$portal_root;
    if(isset($_SESSION["redirecturl"])){ header("Location: $pagelocation/#".$_SESSION["redirecturl"]); }else{ header("Location: $pagelocation"); }
 
+  try{
 
-  // $response = $fb->get('/me?fields=name,email', $accessToken->getValue());
-  // $user = $response->getGraphUser();
-  // $userid = $user['id'];
-  // $revokeCall = '/'. $userid .'/permissions';
+  // access token but useremail not set
+  if(isset($_SESSION['microsoft_access_token']))
+  {
+    if(!isset($_SESSION['useremail']))
+    {
+      $_SESSION['useremail']=$infoObject->preferred_username;
+      $_SESSION['usertype']= 'parent';
+      $_SESSION['displayName']= $infoObject->name;
+      $_SESSION['picture'] = sitesettings('sitelogo');
+    }
+  }else{
+    //header("Location: $pagelocation");
+  }
 
-  // try{
-  //
-  // // access token but useremail not set
-  // if(isset($_SESSION['microsoft_access_token']))
-  // {
-  //   if(!isset($_SESSION['useremail']))
-  //   {
-  //     $_SESSION['useremail']=$user['email'];
-  //     $_SESSION['usertype']= 'parent';
-  //     $_SESSION['displayName']= $user['name'];
-  //     $_SESSION['picture'] = sitesettings('sitelogo');
-  //   }
-  // }else{
-  //   //header("Location: $pagelocation");
-  // }
-  //
-  // if (isset($_SESSION['microsoft_access_token']))
-  // {
-  //   if($_SESSION['usertype']!="")
-  //   {
-  //     include "abre_dbconnect.php";
-  //     if($result=$db->query("SELECT * from users_parent where email='".$_SESSION['useremail']."'"))
-  //     {
-  //       $count=$result->num_rows;
-  //       if($count=='1')
-  //       {
-  //         //If not already logged in, check and get a refresh token
-  //         if (!isset($_SESSION['loggedin'])) { $_SESSION['loggedin']=""; }
-  //         if($_SESSION['loggedin']!="yes")
-  //         {
-  //           //Mark that they have logged in
-  //           $_SESSION['loggedin']="yes";
-  //         }
-  //       }
-  //       else
-  //       {
-  //         $sha1useremail=sha1($_SESSION['useremail']);
-  //         $storetoken=$sha1useremail.$hash;
-  //         mysqli_query($db, "Insert into users_parent (email, students) VALUES ('".$_SESSION['useremail']."', '')") or die (mysqli_error($db));
-  //       }
-  //
-  //     }
-  //     $db->close();
-  //   }
-  //
-  // }
-  // }
-  // catch (Exception $x)
-  // {
-  //
-  // if(strpos($x->getMessage(), 'Invalid Credentials'))
-  // {
-  //
-  //
-  //   session_destroy();
-  //   $fb->api($revokeCall, "DELETE", $accessToken);
-  //   //$client->revokeToken();
-  //
-  //   //Redirect user
-  //   //header("Location: $portal_root");
-  //
-  // }
-  //
-  // if(strpos($x->getMessage(), 'Invalid Credentials'))
-  // {
-  //
-  //   //Destroy the OAuth & PHP session
-  //   session_destroy();
-  //   $fb->api($revokeCall, "DELETE", $accessToken);
-  //   //$client->revokeToken();
-  //
-  //   //Redirect user
-  //   //header("Location: $portal_root");
-  // }
-  //
-  // }
-  //
-  // header("Location: $portal_root");
-  // // User is logged in with a long-lived access token.
-  // // You can redirect them to a members-only page.
-  // //header('Location: https://example.com/members.php');
+  if (isset($_SESSION['microsoft_access_token']))
+  {
+    if($_SESSION['usertype']!="")
+    {
+      include "abre_dbconnect.php";
+      if($result=$db->query("SELECT * from users_parent where email='".$_SESSION['useremail']."'"))
+      {
+        $count=$result->num_rows;
+        if($count=='1')
+        {
+          //If not already logged in, check and get a refresh token
+          if (!isset($_SESSION['loggedin'])) { $_SESSION['loggedin']=""; }
+          if($_SESSION['loggedin']!="yes")
+          {
+            //Mark that they have logged in
+            $_SESSION['loggedin']="yes";
+          }
+        }
+        else
+        {
+          $sha1useremail=sha1($_SESSION['useremail']);
+          $storetoken=$sha1useremail.$hash;
+          mysqli_query($db, "Insert into users_parent (email, students) VALUES ('".$_SESSION['useremail']."', '')") or die (mysqli_error($db));
+        }
+
+      }
+      $db->close();
+    }
+
+  }
+  }
+  catch (Exception $x)
+  {
+
+  if(strpos($x->getMessage(), 'Invalid Credentials'))
+  {
+
+
+    session_destroy();
+
+    //Redirect user
+    header("Location: $portal_root");
+
+  }
+
+  if(strpos($x->getMessage(), 'Invalid Credentials'))
+  {
+
+    //Destroy the OAuth & PHP session
+    session_destroy();
+
+    //Redirect user
+    header("Location: $portal_root");
+  }
+
+  }
+
+  header("Location: $portal_root");
+  // User is logged in with a long-lived access token.
+  // You can redirect them to a members-only page.
+  //header('Location: https://example.com/members.php');
 
 ?>
