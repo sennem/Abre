5765de0c3a62b1f1b0c2b4bf335bb9aa0e7361ae
Brandon Wilson <wilso199@miamioh.edu>
2017-05-09 15:54:18 -0400

    Styling changes to parent login page

--- modules/stream/stream.php
+++ modules/stream/stream.php
@@ -1,31 +1,31 @@
 <?php
-	
+
 	/*
-	* Copyright 2015 Hamilton City School District	
-	* 		
+	* Copyright 2015 Hamilton City School District
+	*
 	* This program is free software: you can redistribute it and/or modify
     * it under the terms of the GNU General Public License as published by
     * the Free Software Foundation, either version 3 of the License, or
     * (at your option) any later version.
-	* 
+	*
     * This program is distributed in the hope that it will be useful,
     * but WITHOUT ANY WARRANTY; without even the implied warranty of
     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     * GNU General Public License for more details.
-	* 
+	*
     * You should have received a copy of the GNU General Public License
     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
     */
-	
+
 	//Required configuration files
 	require(dirname(__FILE__) . '/../../configuration.php');
-	require_once(dirname(__FILE__) . '/../../core/abre_verification.php'); 
-	require_once(dirname(__FILE__) . '/../../core/abre_functions.php'); 
-	require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');	
-	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');	
-	
+	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+
 	$StreamEnd=$_GET["StreamEnd"];
-	
+
 	//Get User Settings
 	$query = "SELECT * FROM profiles where email='".$_SESSION['useremail']."'";
 	$gafecards = databasequery($query);
@@ -44,25 +44,28 @@
 		if(!empty($setting_card_calendar)==1 or empty($gafecards)){ echo "<div id='streamcalendar'>"; include "../calendar/load.php"; echo "</div>"; }
 		if(!empty($setting_card_classroom)==1 or empty($gafecards)){ echo "<div class='hide-on-small-only'><div id='streamclassroom'>"; include "../classroom/load.php"; echo "</div></div>"; }
 		if(!empty($setting_card_apps)==1 or empty($gafecards)){ echo "<div class='hide-on-small-only'><div id='streamapps'>"; include "../apps/load.php"; echo "</div></div>"; }
+		if($_SESSION['usertype'] == 'parent'){
+			echo "<div class='row center-align'><div class='col s12'><h6>Parent Access Coming Soon!</h6></div><div class='col s12'>Soon you will able to access student documents and grades via Abre!</div></div>"; 
+		}
 		echo "<div id='streamcards'></div>";
 	echo "</div>";
-	
+
 	echo "<div style='height:80px;'><div id='showmorestream' style='display:none; position:absolute; left:50%; padding:20px; margin-left:-35px;'><div class='mdl-spinner mdl-js-spinner is-active'></div></div></div>";
 
 ?>
 
 <script>
-	
+
 	$(function()
 	{
-	
+
 		//Hide Mail Until Streams Load
 		$("#streammail").hide();
 		$("#streamdrive").hide();
 		$("#streamcalendar").hide();
 		$("#streamclassroom").hide();
 		$("#streamapps").hide();
-	
+
 		//Load Masonry
 		function checkWidth()
 		{
@@ -78,14 +81,14 @@
 		}
 		checkWidth();
 		$(window).resize(checkWidth);
-		
+
 		//Load Streams
 		var loopcounter=0;
 		var streamCount=<?php echo $StreamEnd; ?>;
 		function loadCards(streamEndCount)
 		{
 			loadingStream=1;
-			$('#streamcards').load('modules/stream/stream_feeds.php?StreamEndResult='+streamEndCount, function () {	
+			$('#streamcards').load('modules/stream/stream_feeds.php?StreamEndResult='+streamEndCount, function () {
 				$('#showmorestream').hide();
 				if(loopcounter==0)
 				{
@@ -103,53 +106,53 @@
 				mdlregister();
 				loadingStream=0;
 			});
-			
+
 			//Refresh
 			setTimeout(function() {
 				loadCards(streamCount);
 			}, 300000)
-			
+
 		}
 		loadCards(streamCount);
-		
+
 		function loadOtherCards()
 		{
 				//Google Classroom
 				<?php if(!empty($setting_card_classroom)==1 or empty($gafecards)){ ?>
 					function loadClassroom() {
-						$('#classroom').load("modules/classroom/card.php", function () {	
+						$('#classroom').load("modules/classroom/card.php", function () {
 							if ($(window).width() >= 600){ $('.grid').masonry( 'reloadItems' ); $('.grid').masonry( 'layout' ); }
 						});
 					}
 					loadClassroom();
 				<?php } ?>
-				
+
 				//Google Drive
 				<?php if(!empty($setting_card_drive)==1 or empty($gafecards)){ ?>
 					function loadDrive() {
-						$('#drive').load("modules/drive/card.php", function () {	
+						$('#drive').load("modules/drive/card.php", function () {
 							if ($(window).width() >= 600){ $('.grid').masonry( 'reloadItems' ); $('.grid').masonry( 'layout' ); }
 						});
 						setTimeout(loadDrive, 600000);
 					}
 					loadDrive();
 				<?php } ?>
-					
+
 				//Google Calendar
 				<?php if(!empty($setting_card_calendar)==1 or empty($gafecards)){ ?>
 					function loadCalendar() {
-						$('#calendar').load("modules/calendar/card.php", function () {	
+						$('#calendar').load("modules/calendar/card.php", function () {
 							if ($(window).width() >= 600){ $('.grid').masonry( 'reloadItems' ); $('.grid').masonry( 'layout' ); }
 						});
 						setTimeout(loadCalendar, 600000);
 					}
 					loadCalendar();
 				<?php } ?>
-					
+
 				//Google Mail
 				<?php if(studentaccess()==true){ if(!empty($setting_card_mail)==1 or empty($gafecards)){ ?>
 					function loadMail() {
-						$('#mail').load("modules/mail/card.php", function () {	
+						$('#mail').load("modules/mail/card.php", function () {
 							if ($(window).width() >= 600){ $('.grid').masonry( 'reloadItems' ); $('.grid').masonry( 'layout' ); }
 						});
 						setTimeout(loadMail, 180000);
@@ -159,9 +162,9 @@
 					}
 					}
 				?>
-			
+
 		}
-	
+
 		//Check card for updates once email clicked
 		$(document).on("click", ".emailclick", function ()
 		{
@@ -169,8 +172,8 @@
 			{
 	        	loadOtherCards();
 	        }, 5000);
-		});	
-		
+		});
+
 		//Load more when at bottom of page
 		$(".mdl-layout__content").unbind('scroll');
 		$(".mdl-layout__content").bind('scroll', function ()
@@ -185,16 +188,16 @@
 			   	}
 		    }
 	    });
-		
-	});	
-	
+
+	});
+
 	//Re-Order Cards
 	function loadOtherCardsApps()
 	{
 		//Apps Card
 		<?php if(!empty($setting_card_apps)==1 or empty($gafecards)){ ?>
 			function loadApps() {
-				$('#apps').load("modules/apps/card.php", function () {	
+				$('#apps').load("modules/apps/card.php", function () {
 					if ($(window).width() >= 600){ $('.grid').masonry( 'reloadItems' ); $('.grid').masonry( 'layout' ); }
 				});
 			}
