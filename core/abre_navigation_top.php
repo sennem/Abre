52b14898b3ee12d5d9a85a955d150bf0e1289248
Brandon Wilson <wilso199@miamioh.edu>
2017-05-15 15:14:16 -0400

    Added ability to add a student via token parent access

--- core/abre_navigation_top.php
+++ core/abre_navigation_top.php
@@ -21,6 +21,26 @@
 	require_once('abre_verification.php');
 
 ?>
+<?php
+if($_SESSION['usertype'] == 'parent'){
+?>
+	<!-- Student Token Modal -->
+	<div id="verifystudent" class="modal modal-fixed-footer modal-mobile-full">
+		<form class="col s12" id="form-verifystudent" method="post" action="<?php echo basename(__DIR__); ?>/verifystudent_process.php">
+		<div class="modal-content">
+			<h4>Verify Your Student</h4>
+			<a class="modal-close black-text" style='position:absolute; right:20px; top:25px;'><i class='material-icons'>clear</i></a>
+			<div class="input-field col s6">
+				<input id="studenttoken" name="studenttoken" type="text" maxlength="20" placeholder="Enter your student token" autocomplete="off" required>
+			</div>
+			<div id="errormessage" style="color:#F44336"></div>
+			</div>
+			<div class="modal-footer">
+			<button type="submit" class="modal-action waves-effect btn-flat white-text" style='background-color: <?php echo sitesettings("sitecolor");?>'>Verify</button>
+		</div>
+		</form>
+	</div>
+<?php } ?>
 
 	<!--Display top navigation-->
 	<header class='mdl-layout__header mdl-color-text--white' style='background-color: <?php echo sitesettings("sitecolor"); ?>'>
@@ -30,16 +50,15 @@
 				<?php
 					if(!isset($_GET["dash"]))
 					{
+						if($_SESSION['usertype'] == 'parent'){
+							echo "<a id='addstudent' class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon modal-verifystudent' href='#verifystudent'><i class='material-icons' style='margin-right:10px'>add</i></a>";
+						}
 						echo "<a class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon modal-viewapps' href='#viewapps'><i class='material-icons'>apps</i></a>";
 					}
 				?>
 				<div class='navspace'></div>
 				<?php
-					// if($_SESSION['usertype'] == 'parent'){
-					// 	echo "<a href='#viewprofile' class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon modal-viewprofile'><img src='".sitesettings('sitelogo')."?sz=120' class='img-center' style='width:32px; height:32px;'></a>";
-					// }else{
 						echo "<a href='#viewprofile' class='mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon modal-viewprofile'><img src='".$_SESSION['picture']."?sz=120' class='img-center' style='width:32px; height:32px;'></a>";
-					// }
 				?>
 			</div>
 		<div id='navigation_top'></div>
@@ -49,10 +68,47 @@
 
 	$(function()
 	{
+		//Student Token Modal
+		$('.modal-verifystudent').leanModal(
+		{
+			in_duration: 0,
+			out_duration: 0,
+			 ready: function() { $("#studenttoken").focus(); }
+		});
+
 		//Scroll to top
 		$('.mdl-layout__header-row').click(function()
 		{
 			$('.mdl-layout__content').animate({ scrollTop: 0 }, 'fast');
+		});
+
+		var formverifystudent = $('#form-verifystudent');
+		$(formverifystudent).submit(function(event) {
+			event.preventDefault();
+			var formData = $(formverifystudent).serialize();
+			$.ajax({
+				type: 'POST',
+				url: $(formverifystudent).attr('action'),
+				data: formData
+			})
+
+			//Show the notification
+			.done(function(response) {
+				var notification = document.querySelector('.mdl-js-snackbar');
+				var status = response.status;
+				if(status == "Success"){
+					$("#studenttoken").val('');
+					$('#verifystudent').closeModal({
+						in_duration: 0,
+						out_duration: 0,
+					});
+					var data = { message: response.message };
+					notification.MaterialSnackbar.showSnackbar(data);
+				}
+				if(status == "Error"){
+					$('#errormessage').text(response.message);
+				}
+			});
 		});
 	});
 
