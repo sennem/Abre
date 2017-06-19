7926bcdb5354594a723275ce6ace59dd82a6b0f3
Brandon Wilson <wilso199@miamioh.edu>
2017-06-18 20:07:41 -0400

    Modified calendar update so that it modifies the json object stored in the database for the current year while leaving other years untouched.

--- modules/profile/calendar_update.php
+++ modules/profile/calendar_update.php
@@ -1,23 +1,28 @@
 <?php
-	
-	
+
+
 	//Required configuration files
-	require(dirname(__FILE__) . '/../../configuration.php'); 
+	require(dirname(__FILE__) . '/../../configuration.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_google_login.php');
 
 	$calendardaystosave=$_POST["calendardaystosave"];
+	$year = $_POST["year"];
+	$json = $_POST["jsonDates"];
+
+	$replacement = array($year => $calendardaystosave);
+
+	$ret = array_replace($json, $replacement);
+	$ret = json_encode($ret);
 
 	include "../../core/abre_dbconnect.php";
 	$stmt = $db->stmt_init();
-	$sql = "UPDATE profiles set work_calendar='$calendardaystosave' where email='".$_SESSION['useremail']."'";
+	$sql = "UPDATE profiles set work_calendar='$ret' where email='".$_SESSION['useremail']."'";
 	$stmt->prepare($sql);
 	$stmt->execute();
 	$stmt->store_result();
 	$num_rows = $stmt->num_rows;
 	$stmt->close();
 	$db->close();
-		
-	echo $calendardaystosave;
-	
+
 ?>
