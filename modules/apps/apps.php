a428d113d012dbbc92b822b845c6a1095a62b99f
Brandon Wilson <wilso199@miamioh.edu>
2017-10-01 22:06:56 -0400

    Modified student apps display so that students can move their apps

--- modules/apps/apps.php
+++ modules/apps/apps.php
@@ -110,28 +110,28 @@
 				}
 				else
 				{
-						$sql = "SELECT * FROM apps order by sort";
-						$result = $db->query($sql);
+					$sql = "SELECT * FROM apps order by sort";
+					$result = $db->query($sql);
 
-							while($row = $result->fetch_assoc())
-							{
-								$id=htmlspecialchars($row["id"], ENT_QUOTES);
-								$title=htmlspecialchars($row["title"], ENT_QUOTES);
-								$image=htmlspecialchars($row["image"], ENT_QUOTES);
-								$link=htmlspecialchars($row["link"], ENT_QUOTES);
-								echo "<li id='item_$id' class='col s4 app' style='display:block; height:110px; overflow:hidden; word-wrap: break-word; margin:0 0 10px 0 !important;'>";
-									echo "<img src='$portal_root/core/images/$image' class='appicon_modal'>";
-									echo "<span><a href='$link' class='applink truncate' style='display:block;'>$title</a></span>";
-								echo "</li>";
-							}
+						while($row = $result->fetch_assoc())
+						{
+							$id=htmlspecialchars($row["id"], ENT_QUOTES);
+							$title=htmlspecialchars($row["title"], ENT_QUOTES);
+							$image=htmlspecialchars($row["image"], ENT_QUOTES);
+							$link=htmlspecialchars($row["link"], ENT_QUOTES);
+							echo "<li id='item_$id' class='col s4 app' style='display:block; height:110px; overflow:hidden; word-wrap: break-word; margin:0 0 10px 0 !important;'>";
+								echo "<img src='$portal_root/core/images/$image' class='appicon_modal'>";
+								echo "<span><a href='$link' class='applink truncate' style='display:block;'>$title</a></span>";
+							echo "</li>";
+						}
+					}
 				}
 			}
-	    }
 	    echo "</ul>";
 	    echo "</div>";
 	}
 
-	//Display student apps
+	//Display parent and student apps
 	if($_SESSION['usertype'] == 'parent'){
 		echo "<div class='row'><p style='text-align:center; font-weight:600;'>Parent Apps</p><hr style='margin-bottom:20px;'>";
 		$sql2 = "SELECT * FROM apps WHERE parent = 1 AND required = 1 order by sort";
@@ -146,18 +146,108 @@
 		}
 		echo "</div>";
 	}else{
+		//Display student apps
 		echo "<div class='row'><p style='text-align:center; font-weight:600;'>Student Apps</p><hr style='margin-bottom:20px;'>";
-		$sql2 = "SELECT * FROM apps WHERE student = 1 AND required = 1 order by sort";
-		$result2 = $db->query($sql2);
-		while($row2 = $result2->fetch_assoc())
+		$sql = "SELECT * FROM apps WHERE student = 1 AND required = 1 order by sort";
+		$result = $db->query($sql);
+		$item=array();
+		while($row = $result->fetch_assoc())
 		{
-			$id=htmlspecialchars($row2["id"], ENT_QUOTES);
-			$title=htmlspecialchars($row2["title"], ENT_QUOTES);
-			$image=htmlspecialchars($row2["image"], ENT_QUOTES);
-			$link=htmlspecialchars($row2["link"], ENT_QUOTES);
-			echo "<div class='col s4 app' style='display:block; height:110px; overflow:hidden; word-wrap: break-word; margin:0 0 10px 0 !important;'><div><img src='$portal_root/core/images/$image' class='appicon_modal'></div><span><a href='$link' class='applink truncate' style='display:block;'>$title</a></span></div>";
-		}
-		echo "</div>";
+			$title=htmlspecialchars($row["title"], ENT_QUOTES);
+			$image=htmlspecialchars($row["image"], ENT_QUOTES);
+			$link=htmlspecialchars($row["link"], ENT_QUOTES);
+			$minor_disabled=htmlspecialchars($row["minor_disabled"], ENT_QUOTES);
+			echo "<ul class='appssort'>";
+			if((studentaccess()!=false) or ($minor_disabled!=1))
+			{
+				$required=array();
+
+				//Get App preference settings (if they exist)
+				$sql2 = "SELECT * FROM profiles where email='".$_SESSION['useremail']."'";
+				$result2 = $db->query($sql2);
+				$apps_order=NULL;
+				while($row2 = $result2->fetch_assoc()) {
+					$apps_order=htmlspecialchars($row2["apps_order"], ENT_QUOTES);
+				}
+
+				//Build Array of Required Apps
+				$sql = "SELECT * FROM apps WHERE ".$_SESSION['usertype']." = 1 AND required = 1";
+				$result = $db->query($sql);
+				while($row = $result->fetch_assoc())
+				{
+					$id=htmlspecialchars($row["id"], ENT_QUOTES);
+					array_push($required, $id);
+				}
+
+				//Display default order, unless they have saved prefrences
+				if($apps_order!=NULL)
+				{
+					$order = explode(',', $apps_order);
+				}
+				else
+				{
+					$order=array();
+				}
+
+				//Compare
+				foreach($required as $key => $requiredvalue)
+				{
+					$hit=NULL;
+					foreach($order as $key => $ordervalue)
+					{
+						if($requiredvalue==$ordervalue)
+						{
+							$hit="yes";
+						}
+					}
+
+					if($hit==NULL)
+					{
+						array_push($order, $requiredvalue);
+					}
+				}
+
+				if($apps_order!=NULL)
+				{
+					foreach($order as $key => $value)
+					{
+						$sql = "SELECT * FROM apps WHERE id='$value'";
+						$result = $db->query($sql);
+
+							while($row = $result->fetch_assoc())
+							{
+								$id=htmlspecialchars($row["id"], ENT_QUOTES);
+								$title=htmlspecialchars($row["title"], ENT_QUOTES);
+								$image=htmlspecialchars($row["image"], ENT_QUOTES);
+								$link=htmlspecialchars($row["link"], ENT_QUOTES);
+								echo "<li id='item_$id' class='col s4 app' style='display:block; height:110px; overflow:hidden; word-wrap: break-word; margin:0 0 10px 0 !important;'>";
+									echo "<img src='$portal_root/core/images/$image' class='appicon_modal'>";
+									echo "<span><a href='$link' class='applink truncate' style='display:block;'>$title</a></span>";
+								echo "</li>";
+							}
+					}
+				}
+				else
+				{
+					$sql = "SELECT * FROM apps order by sort";
+					$result = $db->query($sql);
+
+						while($row = $result->fetch_assoc())
+						{
+							$id=htmlspecialchars($row["id"], ENT_QUOTES);
+							$title=htmlspecialchars($row["title"], ENT_QUOTES);
+							$image=htmlspecialchars($row["image"], ENT_QUOTES);
+							$link=htmlspecialchars($row["link"], ENT_QUOTES);
+							echo "<li id='item_$id' class='col s4 app' style='display:block; height:110px; overflow:hidden; word-wrap: break-word; margin:0 0 10px 0 !important;'>";
+								echo "<img src='$portal_root/core/images/$image' class='appicon_modal'>";
+								echo "<span><a href='$link' class='applink truncate' style='display:block;'>$title</a></span>";
+							echo "</li>";
+						}
+					}
+				}
+			}
+	    echo "</ul>";
+	    echo "</div>";
 	}
 
 	//Display Apps Editor if superadmin
