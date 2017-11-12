881309cb00c166f397fe11819467b7371d75fae2
Brandon Wilson <wilso199@miamioh.edu>
2017-11-12 18:07:01 -0500

    Added new parent table to reference id of users_parent table and store the students they are verified to access

--- modules/settings/installer.php
+++ modules/settings/installer.php
@@ -28,20 +28,57 @@
 		//Check for users_parents table
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
 		if(!$db->query("SELECT * FROM users_parent")){
-			 $sql = "CREATE TABLE `users_parent` (`id` int(11) NOT NULL,`email` text NOT NULL,`students` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			 $sql = "CREATE TABLE `users_parent` (`id` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 			 $sql .= "ALTER TABLE `users_parent` ADD PRIMARY KEY (`id`);";
 			 $sql .= "ALTER TABLE `users_parent` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
 			 $db->multi_query($sql);
 		}
 		$db->close();
 
+		//check for user email field
 		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-		if(!$db->query("SELECT studentId FROM users_parent")){
-			$sql = "ALTER TABLE `users_parent` ADD `studentId` text NOT NULL;";
+		if(!$db->query("SELECT email FROM users_parent")){
+			$sql = "ALTER TABLE `users_parent` ADD `email` text NOT NULL;";
 			$db->multi_query($sql);
 		}
 		$db->close();
-  }
+
+		//Check for parents_students table
+		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+		if(!$db->query("SELECT * FROM parent_students")){
+			 $sql = "CREATE TABLE `parent_students` (`id` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+			 $sql .= "ALTER TABLE `parent_students` ADD PRIMARY KEY (`id`);";
+			 $sql .= "ALTER TABLE `parent_students` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
+			 $db->multi_query($sql);
+		}
+		$db->close();
+
+		//check for parent_ID field
+		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+		if(!$db->query("SELECT parent_id FROM parent_students")){
+			$sql = "ALTER TABLE `parent_students` ADD `parent_id` int(11) NOT NULL;";
+			$sql .= "ALTER TABLE `parent_students` ADD FOREIGN KEY (`parent_id`) REFERENCES users_parent(`id`);";
+			$db->multi_query($sql);
+		}
+		$db->close();
+
+		//check for student token field
+		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+		if(!$db->query("SELECT student_token FROM parent_students")){
+			$sql = "ALTER TABLE `parent_students` ADD `student_token` text NOT NULL;";
+			$db->multi_query($sql);
+		}
+		$db->close();
+
+		//check for student id field
+		require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+		if(!$db->query("SELECT student_id FROM parent_students")){
+			$sql = "ALTER TABLE `parent_students` ADD `student_id` text NOT NULL;";
+			$db->multi_query($sql);
+		}
+		$db->close();
+	}
+
 
   //Write the Setup File
   $myfile = fopen("$portal_path_root/modules/settings/setup.txt", "w");
