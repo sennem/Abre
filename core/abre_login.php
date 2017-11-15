286c49541ff613f40c468a68d5bb8a2e0ecaa56f
Christopher Rose <crose@hcsdoh.org>
2017-11-15 11:57:29 -0500

    Login UI Update

--- core/abre_login.php
+++ core/abre_login.php
@@ -23,27 +23,47 @@
 
 	<!-- Display the login -->
 	<main>
-	<div class="mdl-layout mdl-js-layout login-card">
-		<div class="login_wrapper">
-			<div class="login-card-square mdl-card animated fadeIn">
-				<div class="mdl-card__title mdl-card--expand" style='width:200px; height:200px; background:url(<?php echo getSiteLogo(); ?>) center center no-repeat; -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover; margin-left:20px; margin-bottom:5px;'></div>
-				<?php
-					echo "<div class='mdl-card-text mdl-color-text--grey-600'>".getSiteLoginText()."</div>";
-
-			  		echo "<a class='waves-effect waves-light btn-large mdl-color-text--white' href='$authUrl' style='margin: 0 auto; width:100%; text-transform:none; background-color:".getSiteColor()."'>Staff and Student Login</a>";
+		
+		<?php
+			
+			//Overlay Div
+			echo "<div id='loadingcover' style='background-color:".getSiteColor()."; position:fixed; width:100%; height:100%; z-index:2'></div>";
+			
+			//Login Holder
+			echo "<div class='mdl-layout mdl-js-layout login-card' style='background-color:".getSiteColor()."; background-image: url(/core/images/abre_pattern.png); '>";
+				echo "<div class='login_wrapper'>";
+					echo "<div class='login-card-square mdl-card'>";
+						
+						//Site Logo
+						echo "<div style='height:240px; background:url(". getSiteLogo() .") center center no-repeat; margin-bottom:10px; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;'></div>";
+						
+						//Site Login Text
+						echo "<div class='mdl-card-text mdl-color-text--grey-600' style='text-align:center;'>".getSiteLoginText()."</div>";
+		
+						//Staff and Student Login
+					  	echo "<a class='waves-effect waves-light btn-large mdl-color-text--white' href='$authUrl' style='margin: 0 auto; width:100%; text-transform:none; background-color:".getSiteColor()."'>Staff and Student Login</a>";
+						
+						//Parent Login	
 						if(getSiteParentAccess() == "checked"){
 							echo "<div style='padding-top: 1em'>";
 								echo "<a class='waves-effect waves-light btn-large mdl-color-text--white' href='?usertype=Parent' style='margin:0 auto; width:100%; text-transform:none; background-color:".getSiteColor()."'>Parent Login</a>";
 							echo "</div>";
 						}
-				?>
-			</div>
-		</div>
-	</div>
+					
+					echo "</div>";
+				echo "</div>";
+			echo "</div>";
+		
+		?>
+	
 	</main>
 
 <script>
-
+	
+	$(function(){
+		
+		$("#loadingcover").delay(300).fadeOut();
+	
 		//Responsive login view
 		function loginWidthCheck(){
 			if ($(window).width() < 600){
@@ -54,8 +74,10 @@
 				$(".login-card").removeClass("mdl-color--white");
 			}
 		}
-
+	
 		loginWidthCheck();
 		$(window).resize(loginWidthCheck);
+		
+	});
 
 </script>
