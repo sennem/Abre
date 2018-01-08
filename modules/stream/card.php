3dac0a80fa1ee08615a1249f76066c867e32de80
Chris Rose <chrislrose@gmail.com>
2018-01-08 14:24:47 -0500

    Updated Stream App

---
+++ modules/stream/card.php
@@ -0,0 +1,107 @@
+<?php
+
+	/*
+	* Copyright (C) 2016-2017 Abre.io LLC
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the Affero General Public License version 3
+    * as published by the Free Software Foundation.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+	*
+    * You should have received a copy of the Affero General Public License
+    * version 3 along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+    */
+
+	//Required configuration files
+	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+
+	//Encode Links
+	$linkbase = base64_encode($linkraw);
+	$linkescaped = htmlspecialchars($linkraw, ENT_QUOTES);
+	$imagebase = base64_encode($image);
+	$displaydate = date("F jS, Y", $date);
+	$titleencoded = base64_encode($title);
+
+	//Display Card
+	echo "<div class='mdl-card mdl-shadow--2dp card_stream hoverable' style='float:left;'>";
+	
+		//Feed
+		echo "<div class='truncate' style='padding:16px 16px 0 16px; font-size: 12px; color: #999; font-weight: 500;'>";
+			echo "<a href='$feedlink' style='color: #999; font-weight: 500;' target='_blank'>$feedtitle</a>";
+		echo "</div>";
+	
+		//Title
+		echo "<div class='cardtitle' style='height:60px; padding:5px 16px 0 16px;'>";
+			echo "<div class='mdl-card__title-text ellipsis-multiline cardclick pointer' data-link='$linkescaped' style='font-weight:700; font-size:20px; line-height:24px;'>$title</div>";
+		echo "</div>";
+		
+		//Date
+		echo "<div class='truncate' style='padding:0 16px 10px 16px; font-size: 12px; color: #999;'>";
+			echo "<a href='$feedlink' style='color: #999; font-weight: 400;' target='_blank'>$displaydate</a>";
+		echo "</div>";
+
+		//Card Image
+		if($image != ""){
+			echo "<div class='mdl-card__media mdl-color--grey-100 mdl-card--expand cardclick pointer' data-link='$linkescaped' style='height:200px; background-image: url($image);'></div>";
+		}
+		else
+		{
+				
+			if ($excerpt!=""){ $body = $excerpt; }else{ $body = $feedtitle; }
+			if (strlen($body) > 100){ 
+				$body = substr( $body, 0, strrpos( substr( $body, 0, 100), ' ' ));
+				$body = substr($body, 0, 97) . ' ...';
+			}
+				
+			echo "<div class='mdl-card__media mdl-color--grey-100 mdl-card--expand valign-wrapper cardclick pointer' data-link='$linkescaped' style='height:200px; background-image: url(/core/images/abre_pattern.png); background-color: ".getSiteColor()." !important; overflow:hidden;'>";
+				echo "<span style='width:100%; color:#fff; padding:32px; font-size:18px; line-height:normal; font-weight:700; text-align:center;'>$body</span>";
+			echo "</div>";
+			
+		}
+
+		//Card Actions
+		echo "<div class='mdl-card__actions'>";
+		
+			//Read Button
+			echo "<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect' href='$linkescaped' style='color: ".getSiteColor()."' target='_blank'>Read</a>";
+
+			//Share, Likes, Comments for Staff Only
+			if($_SESSION['usertype'] == 'staff'){
+				
+				echo "<div class='mdl-layout-spacer'></div>";
+				
+				//Share
+				echo "<a class='material-icons mdl-color-text--grey-600 modal-sharecard commenticon shareinfo' style='margin-right:30px;' data-url='$linkbase' title='Share' href='#sharecard'>share</a>";
+
+				//Likes
+				$query = "SELECT * FROM streams_comments WHERE url = '$link' AND liked = '1' AND user = '".$_SESSION['useremail']."'";
+				$dbreturn = databasequery($query);
+				$num_rows_like_current_user = count($dbreturn);
+				if($num_rows_like == 0){
+					echo "<a class='material-icons mdl-color-text--grey-600 likeicon' data-title='$titleencoded' data-category='$feedtitle' data-excerpt='$excerpt' data-url='$linkbase' data-image='$imagebase' title='Like' href='#'>favorite</a> <span class='mdl-color-text--grey-600' style='font-size:12px; font-weight:600; width:30px; padding-left:5px;'>$num_rows_like</span>";
+				}else{
+					if($num_rows_like_current_user == 0){
+						echo "<a class='material-icons mdl-color-text--grey-600 likeicon' data-title='$titleencoded' data-category='$feedtitle' data-url='$linkbase' data-image='$imagebase' href='#'>favorite</a> <span class='mdl-color-text--grey-600' style='font-size:12px; font-weight:600; width:30px; padding-left:5px;'>$num_rows_like</span>";
+					}else{
+						echo "<a class='material-icons mdl-color-text--red likeicon' data-title='$titleencoded' data-category='$feedtitle' data-url='$linkbase' data-image='$imagebase' href='#'>favorite</a> <span class='mdl-color-text--red' style='font-size:12px; font-weight:600; width:30px; padding-left:5px;'>$num_rows_like</span>";
+					}
+				}
+
+				//Comments
+				if($num_rows_comment == 0){
+					echo "<a class='material-icons mdl-color-text--grey-600 modal-addstreamcomment commenticon' data-commenticonid='comment_$cardcountloop' data-image='$imagebase' data-redirect='latest' data-title='$titleencoded' data-category='$feedtitle' data-excerpt='$excerpt' data-url='$linkbase' title='Add a comment' href='#addstreamcomment'>insert_comment</a><span id='comment_$cardcountloop' style='font-size:12px; font-weight:600; width:30px; padding-left:5px; color:grey'>$num_rows_comment</span>";
+				}else{
+					echo "<a class='material-icons modal-addstreamcomment commenticon' style='color: ".getSiteColor().";' data-commenticonid='comment_$cardcountloop' data-image='$imagebase' data-redirect='latest' data-title='$titleencoded' data-category='$feedtitle' data-excerpt='$excerpt' data-url='$linkbase' title='Add a comment' href='#addstreamcomment'>insert_comment</a> <span id='comment_$cardcountloop' style='font-size:12px; font-weight:600; width:30px; padding-left:5px; color: ".getSiteColor()."'>$num_rows_comment</span>";
+				}
+			}
+
+		echo "</div>";
+
+	echo "</div>";
+
+?>
