b05d07dc748d2ec44ec6842032c60c2782f45e87
Chris Rose <chrislrose@gmail.com>
2018-01-08 14:25:04 -0500

    Updated Stream App

--- modules/stream/modals.php
+++ modules/stream/modals.php
@@ -46,6 +46,9 @@
 					</div>
 					<input type="hidden" name="streamUrl" id="streamUrl">
 					<input type="hidden" name="streamTitleValue" id="streamTitleValue">
+					<input type="hidden" name="commentID" id="commentID">
+					<input type="hidden" name="streamImage" id="streamImage">
+					<input type="hidden" name="redirect" id="redirect">
 		    </div>
 			  <div class="modal-footer">
 					<button class="btn waves-effect btn-flat white-text" type="submit" name="action" style='margin-left:5px; background-color:<?php echo getSiteColor(); ?>'>Post</button>
@@ -64,8 +67,72 @@
 					<div class="socialshare pointer" style="width:100%; background-color:#d34836; padding:15px; text-align: center; color:#fff; font-size:16px;" data-link="https://plus.google.com/share?url=">Google</div>
 					<input type="hidden" name="share_url" id="share_url">
 				</div>
-	   </form>
-	 </div>
+	   		</form>
+	 	</div>
+	 	
+		<div id="editwidgets" class="modal modal-mobile-full" style="max-width: 600px;">
+			<form>
+				<div class="modal-content">
+					<h4>Edit Widgets</h4>
+					<a class="modal-close black-text" style='position:absolute; right:20px; top:25px;'><i class='material-icons'>clear</i></a>
+					
+					<?php
+						
+						//Check to see if there is hidden widgets
+						$sql = "SELECT * FROM profiles WHERE email = '".$_SESSION['useremail']."'";
+						$result = $db->query($sql);
+						$widgets_order = NULL;
+						while($row = $result->fetch_assoc()) {
+							$widgets_hidden = htmlspecialchars($row["widgets_hidden"], ENT_QUOTES);
+						}
+
+						//Show All Widgets
+						echo "<table class='bordered'>";
+						$widgetsdirectory = dirname(__FILE__) . '/../';
+						$widgetsfolders = scandir($widgetsdirectory);
+						foreach($widgetsfolders as $result){
+							
+							$pagetitle = NULL;
+				
+							if(file_exists(dirname(__FILE__) . '/../'.$result.'/widget.php')){
+
+								include(dirname(__FILE__) . '/../'.$result.'/config.php');
+									
+								echo "<tr style='background-color:#f9f9f9'>";
+									echo "<td><b>$pagetitle</b><td>";
+									echo "<td style='width:30px'>";
+								
+										echo "<div class='switch'><label><input type='checkbox' class='widgetclick' name='$result' id='$result' value='1' ";
+										
+										//Check if hidden
+										if($widgets_hidden!=NULL){
+											$HiddenWidgets = explode(',',$widgets_hidden);
+											if(!in_array($result, $HiddenWidgets)){
+											
+												echo " checked ";
+												
+											}
+										}
+										else
+										{
+											echo " checked ";
+										}
+											
+										echo "/><span class='lever'></span></label></div>";
+
+									echo "</td>";
+								echo "</tr>";
+
+							}
+				
+						}
+						echo "</table>";	
+						
+					?>
+					
+				</div>
+	   		</form>
+	 	</div>
 
 	<?php
 	}
@@ -84,12 +151,32 @@
 			var finallink = network+link;
 		    objWindow = window.open(finallink, 'Social Share', 'width=500,height=500,resizable=no').focus();
 		});
+		
+		//Update Widget Setting
+		$(".widgetclick").unbind().click(function(e){
+			var widget = $(this).attr('id');
+			var selectedWidgets = "";
+			
+			$("input:checkbox[class=widgetclick]:not(:checked)").each(function () {
+           		selectedWidgets = $(this).attr("id") + "," + selectedWidgets;
+        	});
+        	
+        	selectedWidgets = selectedWidgets.replace(/^,|,$/g,'');
+
+			$.post("modules/stream/save_widget_status.php", {widgets: selectedWidgets}, function() {
+				$("#streamwidgets").load("modules/<?php echo basename(__DIR__); ?>/widgets.php");
+			});
+			
+		});
 
 		//Save comment
 		var form = $('#form-addstreamcomment');
 		$(form).submit(function(event) {
 			event.preventDefault();
 			$("#commentstatustext").text("Posting comment...");
+			var url = $("#streamUrl").val();
+			var id = $("#commentID").val();
+			var redirect = $("#redirect").val();
 			var formData = $(form).serialize();
 			$.ajax({
 				type: 'POST',
@@ -104,13 +191,20 @@
 					$(".modal-content #streamUrl").val(response);
 					var element = document.getElementById("commentthreadbox");
 					element.scrollTop = element.scrollHeight;
-					$('#streamcards').load("modules/stream/stream_feeds.php", function () {
-						$('.grid').masonry( 'reloadItems' );
-						$('.grid').masonry( 'layout' );
-						mdlregister();
-					});
-					$('#streamlikes').load("modules/stream/stream_likes.php", function () {
-						mdlregister();
+					$.post( "modules/<?php echo basename(__DIR__); ?>/update_card.php", {url: url, redirect: redirect, type: "comment"})
+					.done(function(data) {
+						$("#"+id).prev().removeClass("mdl-color-text--grey-600");
+						$("#"+id).prev().css("color", "<?php echo getSiteColor(); ?>");
+						$("#"+id).css("color", "<?php echo getSiteColor(); ?>");
+						$("#"+id).html(data.count);
+						if(redirect == "comments"){
+							if(data.currentusercount > 0){
+								$("#"+id).closest(".card_stream").show();
+							}
+							if(data.streamcardsleft > 0){
+								$("#noCommentsMessage").hide();
+							}
+						}
 					});
 				});
 			})
