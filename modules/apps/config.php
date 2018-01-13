163e83d666fe916bf7bc88ede792d396f7d652f4
Chris Rose <chrislrose@gmail.com>
2018-01-12 19:24:33 -0500

    Removed un-needed id’s from modal

--- modules/apps/config.php
+++ modules/apps/config.php
@@ -34,7 +34,7 @@
 	<!--Apps modal-->
 	<div id='viewapps_arrow' class='hide-on-small-only'></div>
 	<div id="viewapps" class="modal apps_modal modal-mobile-full">
-		<div class="modal-content" id="modal-content-section">
+		<div class="modal-content">
 			<a class="modal-close black-text hide-on-med-and-up" style='position:absolute; right:20px; top:25px;'><i class='material-icons'>clear</i></a>
 			<div id='loadapps'></div>
     </div>
