9dc96db70dc658176254f4b3df6e5e1c02aedfc9
Brandon Wilson <wilso199@miamioh.edu>
2018-01-17 14:34:42 -0500

    Fixing parent login buttons. Wrong auth url was being used to redirect parents

--- core/abre_button_facebook.php
+++ core/abre_button_facebook.php
@@ -17,4 +17,4 @@
     */
 
 	//Facebook Auth Button
-	echo "<a class='waves-effect waves-light btn-large mdl-color-text--white loginbutton' style='background-color:#3664a2 !important; color:#fff !important;' href='$authUrl'><span class='left loginicon' style='background: url(\"core/images/button_icon_facebook.png\") no-repeat;'></span>Sign in with Facebook</a>";
+	echo "<a class='waves-effect waves-light btn-large mdl-color-text--white loginbutton' style='background-color:#3664a2 !important; color:#fff !important;' href='$loginUrl'><span class='left loginicon' style='background: url(\"core/images/button_icon_facebook.png\") no-repeat;'></span>Sign in with Facebook</a>";
