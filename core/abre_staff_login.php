b532c2d351d793b8922140fb83317ac02818acc8
Brandon Wilson <wilso199@miamioh.edu>
2018-02-25 18:46:34 -0500

    Added conditionals and microsoft button for staff and student login

--- core/abre_staff_login.php
+++ core/abre_staff_login.php
@@ -19,55 +19,60 @@
 	//Include required files
 	require_once(dirname(__FILE__) . '/../configuration.php');
 	require_once('abre_functions.php');
-	
+
 	//Display the Login
 	echo "<main>";
-	
+
 		//Overlay Div
 		echo "<div id='loadingcover' style='background-color:".getSiteColor()."; position:fixed; width:100%; height:100%; z-index:2'></div>";
-	
+
 		//Login Holder
 		echo "<div class='mdl-layout mdl-js-layout login-card' style='background-color:".getSiteColor()."; background-image: url(/core/images/abre/abre_pattern.png); '>";
 
 			echo "<div class='login_wrapper'>";
 				echo "<div class='login-card-square mdl-card'>";
-					
+
 						//Site Logo
 						echo "<div style='margin:30px 40px 10px 45px; height:240px; width:240px; background:url(". getSiteLogo() .") center center no-repeat; margin-bottom:10px; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;'></div>";
-						
+
 						//Site Login Text
 						echo "<div class='mdl-card-text mdl-color-text--grey-600' style='text-align:center; margin:0 40px 10px 40px;'>Choose a sign in service</div>";
-						
+
 						//Logins
-						echo "<div class='loginholder'>"; 
-						
+						echo "<div class='loginholder'>";
+
 							//Display SSO Options
 							echo "<div style='padding-top: 10px'>";
 								include "abre_button_google.php";
 							echo "</div>";
-							
+							if(getSiteMicrosoftSignInGroups('staff') == "checked" && getSiteMicrosoftClientId() !== '' && getSiteMicrosoftClientSecret() !== ''){
+								echo "<div style='padding-top: 10px'>";
+									include "abre_button_microsoft.php";
+								echo "</div>";
+							}
+
 							//Return Text
 							echo "<div class='mdl-color-text--grey-600' style='text-align:center; margin:30px 40px 10px 40px; font-size:13px;'><a href='/' class='mdl-color-text--grey-600' style='font-weight:400'>Return to Homepage</a></span></div>";
-							
+
 						echo "</div>";
-						
+
 				echo "</div>";
 			echo "</div>";
-			
+
 			include "abre_copyright.php";
-			
+
 		echo "</div>";
-	
+
 	echo "</main>";
-	
+
 ?>
 
 <script>
-	
+
 	$(function(){
-		
+
 		$("#loadingcover").delay(300).fadeOut();
-	
+
 		//Responsive login view
 		function loginWidthCheck(){
 			if ($(window).width() < 600){
@@ -78,10 +83,10 @@
 				$(".login-card").removeClass("mdl-color--white");
 			}
 		}
-	
+
 		loginWidthCheck();
 		$(window).resize(loginWidthCheck);
-		
+
 	});
 
 </script>
