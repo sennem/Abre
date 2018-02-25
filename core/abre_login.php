fabfc79c631fe627ddfac2084b2fac95cb872c66
Brandon Wilson <wilso199@miamioh.edu>
2018-02-25 18:43:02 -0500

    Changing conditional to determine if parent login button should be shown

--- core/abre_login.php
+++ core/abre_login.php
@@ -22,61 +22,61 @@
 
 	//Display the login
 	echo "<main>";
-			
+
 			//Overlay Div
 			echo "<div id='loadingcover' style='background-color:".getSiteColor()."; position:fixed; width:100%; height:100%; z-index:2'></div>";
-			
+
 			//Login Holder
 			echo "<div class='mdl-layout mdl-js-layout login-card' style='background-color:".getSiteColor()."; background-image: url(/core/images/abre/abre_pattern.png); '>";
-			
+
 				echo "<div class='login_wrapper' style='z-index:100;'>";
 					echo "<div class='login-card-square mdl-card'>";
-						
+
 						//Site Logo
 						echo "<div style='margin:30px 40px 10px 45px; height:240px; width:240px; background:url(". getSiteLogo() .") center center no-repeat; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover;'></div>";
-						
+
 						//Site Login Text
 						echo "<div class='mdl-card-text mdl-color-text--grey-600' style='text-align:center; margin:0 40px 10px 40px;'>Please sign in to continue</div>";
-						
+
 						//Logins
-						echo "<div class='loginholder_large'>"; 
-		
+						echo "<div class='loginholder_large'>";
+
 							//Staff Login
 							echo "<div style='padding-top: 10px'>";
 						  		echo "<a class='waves-effect waves-light btn-large mdl-color-text--white loginbutton' style='background-color:".getSiteColor()." !important; color:#fff !important; text-align:center;' href='?usertype=Staff'>Sign in as Staff</a>";
 						  	echo "</div>";
-						  	
+
 						  	//Student Login
 						  	echo "<div style='padding-top: 10px'>";
 						  		echo "<a class='waves-effect waves-light btn-large mdl-color-text--white loginbutton' style='background-color:".getSiteColor()." !important; color:#fff !important; text-align:center;' href='?usertype=Student'>Sign in as Student</a>";
 						  	echo "</div>";
-							
-							//Parent Login	
-							if(getSiteParentAccess() == "checked"){
+
+							//Parent Login
+							if(getSiteGoogleSignInGroups('parents') == "checked" || getSiteMicrosoftSignInGroups('parents') == "checked" || getSiteFacebookSignInGroups('parents') == "checked"){
 								echo "<div style='padding-top: 10px'>";
 									echo "<a class='waves-effect waves-light btn-large mdl-color-text--white loginbutton' style='background-color:".getSiteColor()." !important; color:#fff !important; text-align:center;' href='?usertype=Parent'>Sign in as Parent</a>";
 								echo "</div>";
 							}
-							
+
 						echo "</div>";
-					
+
 					echo "</div>";
 				echo "</div>";
-				
+
 				include "abre_copyright.php";
-				
+
 			echo "</div>";
-	
+
 	echo "</main>";
-	
+
 ?>
 
 <script>
-	
+
 	$(function(){
-		
+
 		$("#loadingcover").delay(300).fadeOut();
-	
+
 		//Responsive login view
 		function loginWidthCheck(){
 			if ($(window).width() < 600){
@@ -87,10 +87,10 @@
 				$(".login-card").removeClass("mdl-color--white");
 			}
 		}
-	
+
 		loginWidthCheck();
 		$(window).resize(loginWidthCheck);
-		
+
 	});
 
 </script>
