5b4c5992059c6ad473be4426c396033038d99597
Brandon Wilson <wilso199@miamioh.edu>
2018-03-26 10:53:28 -0400

    Table check for abre_students that was previously crashing installer

--- modules/apps/config.php
+++ modules/apps/config.php
@@ -33,8 +33,10 @@
 	//used for routing after verifying a student.
 	$url = $portal_root .'/#mystudents';
 
-	$sql = "SELECT SchoolCode, SchoolName FROM Abre_Students ORDER BY SchoolCode";
-	$schoolResults = databasequery($sql);
+	if($db->query("SELECT * FROM Abre_Students LIMIT 1")){
+		$sql = "SELECT SchoolCode, SchoolName FROM Abre_Students ORDER BY SchoolCode";
+		$schoolResults = databasequery($sql);
+	}
 ?>
 
 	<!--Apps modal-->
