7bc99df54ec80e21e6003c1fefa35f47c3275b5c
Chris Rose <chrislrose@gmail.com>
2018-03-31 13:58:28 -0400

    Fixed image saving tool to add valid file extentions

--- modules/stream/stream_save_image.php
+++ modules/stream/stream_save_image.php
@@ -46,16 +46,13 @@
 	
 				//Get the name and sanatize the file name
 				$fileExtension = pathinfo($image, PATHINFO_EXTENSION);
-				$file_name = basename($image);
-				$file_name = str_replace("+", "_", $file_name);
-				$file_name = str_replace("%", "_", $file_name);
-				$file_name = str_replace(".", "_", $file_name);
-				$file_name = preg_replace('/[^a-zA-Z0-9_.]/', '', $file_name);
-				$filename = $portal_path_root . "/../$portal_private_root/stream/cache/images/" . $date.$file_name .".$fileExtension";
+				$fileExtension = substr($fileExtension, 0, 3);
+				if($fileExtension == "jpe"){ $fileExtension="jpeg"; }
+				$imagefile = $date."_rss.$fileExtension";
+				$filename = $portal_path_root . "/../$portal_private_root/stream/cache/images/$imagefile";
 	
 				//If it already saved, read from local server
 				if (file_exists($filename)){
-					$imagefile = $date.$file_name.".$fileExtension";
 					$image = $portal_root."/modules/stream/stream_serve_image.php?file=$imagefile&ext=$fileExtension";
 					if(filesize($filename) < 1000){
 						$image = "";
@@ -72,7 +69,7 @@
 							
 							//Save image to server
 							$fileExtension = pathinfo($image, PATHINFO_EXTENSION);
-							$local_file = $portal_path_root . "/../$portal_private_root/stream/cache/images/" . $date.$file_name .".$fileExtension";
+							$local_file = $portal_path_root . "/../$portal_private_root/stream/cache/images/$imagefile";
 							$remote_file = $image;
 							$ch = curl_init();
 							$fp = fopen ($local_file, 'w+');
