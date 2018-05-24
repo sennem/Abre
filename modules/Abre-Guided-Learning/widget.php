293fe9b2299ad2f5f596dfa2fa28b949c242f1cb
Brandon Wilson <wilso199@miamioh.edu>
2018-05-24 12:50:14 -0400

    Abre-176: Adding Guided Learning widget for staff

---
+++ modules/Abre-Guided-Learning/widget.php
@@ -0,0 +1,28 @@
+<?php
+
+	/*
+	* Copyright (C) 2016-2018 Abre.io Inc.
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the Affero General Public License version 3
+    * as published by the Free Software Foundation.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+	*
+    * You should have received a copy of the Affero General Public License
+    * version 3 along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+    */
+
+	//Required configuration files
+	require(dirname(__FILE__) . '/../../configuration.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+  require_once(dirname(__FILE__) . '/../../modules/stream/functions.php');
+
+  if(($_SESSION['usertype'] == "staff") && isAppActive("Abre-Guided-Learning")){
+    DisplayWidget('Abre-Guided-Learning','directions','Guided Learning','#00BCD4', $portal_root.'/#guide', false);
+  }
+
+?>
