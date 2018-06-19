929efcb3f41ce81fc2fa07428d21fbe201cbe48d
sennem <marksenne000@gmail.com>
2018-06-19 10:28:58 -0400

    continue debug

--- modules/Abre-Moods/db_submission.php
+++ modules/Abre-Moods/db_submission.php
@@ -3,20 +3,23 @@
   $username = "root";
   $password = "killerm111";
   $dbname = "abredb";
-  $emojivaluetest = $_GET['moodval'];
-  echo $emojivaluetest;
+  $emojiurlval = $_GET['moodval'];
   //Required configuration files
 	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
-  echo 'passed';
-  /*// Create connection
+  //^^^ALL ABOVE WORKS^^
+
+  // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
   }
-
+  else {
+      conn->close();
+  }
+  /*
   $userid=finduseridcore($_SESSION['useremail']);
   $emojimood=$_GET['moodval'];
   $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('$userid','2018-11-11','$emojimood')";
@@ -28,10 +31,6 @@
 
   $conn->close();
 
-  //Required configuration files
-	//require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
-	//require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-	//require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
-*/
-echo 'ok it links';
+
+  */
 ?>
