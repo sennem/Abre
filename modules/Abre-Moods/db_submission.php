e6cf64df1752357e43d252b15ef27e6ccc534487
sennem <marksenne000@gmail.com>
2018-06-19 10:29:56 -0400

    debug

--- modules/Abre-Moods/db_submission.php
+++ modules/Abre-Moods/db_submission.php
@@ -13,12 +13,12 @@
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
-  else {
-      conn->close();
-  }
+  //if ($conn->connect_error) {
+  //    die("Connection failed: " . $conn->connect_error);
+  //}
+  //else {
+  conn->close();
+  //}
   /*
   $userid=finduseridcore($_SESSION['useremail']);
   $emojimood=$_GET['moodval'];
