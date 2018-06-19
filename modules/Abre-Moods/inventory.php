98d789e2ed1a61c6f4357f50c1dba49bc2a81cd6
sennem <marksenne000@gmail.com>
2018-06-19 13:08:06 -0400

    try history

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -19,10 +19,36 @@
 	//Required configuration files
 	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
 	require(dirname(__FILE__) . '/../../configuration.php');
+	//require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
 	require_once('permissions.php');
-	echo 'HEre';
-	if($pagerestrictions=="")
-	{
-		echo "<div id='displaybooks'>"; include "inventory_display.php"; echo "</div>";
-	}
+
+  //$stmt = $db->stmt_init();
+	//$stmt->prepare($sql);
+	//$stmt->execute();
+	//$stmt->close();
+	//$db->close()
+
+	$con=mysqli_connect("localhost","root","killerm111","abredb");
+	// Check connection
+	if (mysqli_connect_errno())
+  {
+  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+	$sql = "SELECT COUNT(*) FROM mood_table";
+	$result = mysqli_query($con,$sql);
+	$rows = mysqli_fetch_row($result);
+	echo $rows[0];
+	echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>Mood History</span><br><p style='font-size:16px; margin:20px 0 0 0;'>View your modd history here.</p></div>";
+	//for (int $i=0; i<$totalrows;i++)
+	//{
+	//
+	//}
+
+
+	//this if keeps me displaying. (it doesnt pass the if test)
+	//if($pagerestrictions=="")
+	//{
+	//	echo "<div id='displaybooks'>"; include "inventory_display.php"; echo "</div>";
+	//}
+
 ?>
