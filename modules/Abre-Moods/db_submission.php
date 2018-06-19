8d68f30d0f4a4ecfeec0ac60e304e838f1e4e168
sennem <marksenne000@gmail.com>
2018-06-19 11:43:40 -0400

    get date and such working

--- modules/Abre-Moods/db_submission.php
+++ modules/Abre-Moods/db_submission.php
@@ -1,20 +1,17 @@
 <?php
+  //Required configuration files
+  require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+  require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+  require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
   $servername = "localhost";
   $username = "root";
   $password = "killerm111";
   $dbname = "abredb";
   $emojimood = $_GET['moodval'];
-  //Required configuration files
-	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
-	require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
-  //^^^ALL ABOVE WORKS^^
-
-  //$userid=finduseridcore($_SESSION['useremail']);
-  //$userid=finduseridcore($_SESSION['useremail']);
+  $date = date('Y-m-d H:i:s');
+  $userid=finduseridcore($_SESSION['useremail']);
   $stmt = $db->stmt_init();
-  //echo $userid;
-  $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('marksenne000@gmail.com','2018-11-11','3')";
+  $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('$userid','$date','$moodval')";
   $stmt->prepare($sql);
   $stmt->execute();
   $stmt->close();
