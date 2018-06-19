07b1227b74a3a54ac9daa79fd92dc511ef23b21d
sennem <marksenne000@gmail.com>
2018-06-19 10:33:14 -0400

    debug 500

--- modules/Abre-Moods/db_submission.php
+++ modules/Abre-Moods/db_submission.php
@@ -3,15 +3,14 @@
   $username = "root";
   $password = "killerm111";
   $dbname = "abredb";
-  $emojiurlval = $_GET['moodval'];
+  $emojimood = $_GET['moodval'];
   //Required configuration files
 	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
 	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
   //^^^ALL ABOVE WORKS^^
 
-  $userid=finduseridcore($_SESSION['useremail']);
-  $emojimood=$_GET['moodval'];
+  //$userid=finduseridcore($_SESSION['useremail']);
   $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('$userid','2018-11-11','$emojimood')";
   $conn->close();
 
