293cb48c6920eb47178635395abbcb8bf3e29b6a
sennem <marksenne000@gmail.com>
2018-06-19 12:01:14 -0400

    try it

--- modules/Abre-Moods/db_submission.php
+++ modules/Abre-Moods/db_submission.php
@@ -4,14 +4,14 @@
   $password = "killerm111";
   $dbname = "abredb";
   $emojimood = $_GET['moodval'];//works
-  $datevar = date('Y-m-d H:i:s');//works
+  //$datevar = date('Y-m-d H:i:s');//works
   //$userid=finduseridcore($_SESSION['useremail']);
   //Required configuration files
   require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
   require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
   require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
   $stmt = $db->stmt_init();
-  $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('marksenne000@gmail.com,'$datevar','$emojimood')";
+  $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('marksenne000@gmail.com,'date','$emojimood')";
   $stmt->prepare($sql);
   $stmt->execute();
   $stmt->close();
