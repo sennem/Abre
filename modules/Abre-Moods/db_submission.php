031469cbb4529a8c464750fba43d92dee2e3ab20
sennem <marksenne000@gmail.com>
2018-06-19 11:46:42 -0400

    work please

--- modules/Abre-Moods/db_submission.php
+++ modules/Abre-Moods/db_submission.php
@@ -1,8 +1,4 @@
 <?php
-  //Required configuration files
-  require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
-  require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
-  require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
   $servername = "localhost";
   $username = "root";
   $password = "killerm111";
@@ -10,8 +6,12 @@
   $emojimood = $_GET['moodval'];
   //$date = date('Y-m-d H:i:s');
   //$userid=finduseridcore($_SESSION['useremail']);
+  //Required configuration files
+  require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+  require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+  require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
   $stmt = $db->stmt_init();
-  $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('marksenne000@mgmail.com','2018-10-11','$moodval')";
+  $sql="INSERT INTO mood_table (Email, Date, Feeling) VALUES ('marksenne000@mgmail.com','2018-10-11','4')";
   $stmt->prepare($sql);
   $stmt->execute();
   $stmt->close();
