8df73bf58aa4d69919b773ee94245e114af8aa6c
sennem <marksenne000@gmail.com>
2018-06-20 13:28:50 -0400

    grr

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -118,7 +118,7 @@
 			echo '<i class="em em-expressionless EmojiSpacingLeft" ></i> - ';
 		}
 		//echo ($row[1]); //just goes to next thing, effectively a counter //or maybe dont need??
-		echo $row['Daterow'][0];
+		echo $row[0]['Daterow'];
 	}
 	$con->close();
 
