9330f65fc1a49e820cb200f42388e4548fd0e465
sennem <marksenne000@gmail.com>
2018-06-20 09:38:31 -0400

    ads

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -48,9 +48,9 @@
 	echo '<br>';
 	while($row = mysqli_fetch_array($result, MYSQLI_NUM))
 	{
-		echo "ROW0";
+		echo "row0>";
 		echo ($row[0]);
-		echo "ROW1";
+		echo "row1>";
 		echo ($row[1]);
 	}
 	$con->close();
