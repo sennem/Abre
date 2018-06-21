cfd2d9d1cc570d26d0f7f35695799cd65081f971
sennem <marksenne000@gmail.com>
2018-06-21 14:41:07 -0400

    sadf

--- modules/Abre-Moods/books_display.php
+++ modules/Abre-Moods/books_display.php
@@ -201,21 +201,7 @@
 				</ul>
 			</div>'
 
-			echo '<br>';
-			if($dateday!=$currentday)
-			{
-				echo 'HIT IF 1';
-				echo "<script>";
-		    echo "document.getElementById('emojizero').style.backgroundColor = 'Red';";
-		    echo "</script>";
-			}
-			elseif(($dateday==$currentday) && ($datemonth!=$currentmonth))
-			{
-				echo 'HIT IF 1';
-				echo "<script>";
-				echo "resetdisp();";
-				echo "</script>";
-			}
+			
 
 			?>
 
