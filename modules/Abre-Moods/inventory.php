ebe66af635203db6e9728a983b8dbdf1005e2d08
sennem <marksenne000@gmail.com>
2018-06-21 10:12:09 -0400

    WHY IS IT SAYING ARRAY

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -51,7 +51,7 @@
 
 
 	echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>Record</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Here you see your mood history.</p></div>";
-	echo '9'; //testing to identify if the page is running off of new saved code
+	echo '10'; //testing to identify if the page is running off of new saved code
 	echo '<br>';
 
 	//---$con=mysqli_connect("localhost","root","killerm111","abredb");
@@ -82,7 +82,7 @@
   echo 'NO RESULTS';
 	}
 	//---$counterfeeling=0;
-	$rowsfeeling=array();
+	//$rowsfeeling=array();
 	while($rowfeeling=mysqli_fetch_row($resultfeeling))
 	{
 		$rowsfeeling[]=$rowfeeling;
