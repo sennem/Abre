418f954ebf5a6587e16fc25dfa9ade8e6da5da74
sennem <marksenne000@gmail.com>
2018-06-21 10:16:31 -0400

    WHY

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -51,7 +51,7 @@
 
 
 	echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>Record</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Here you see your mood history.</p></div>";
-	echo '13'; //testing to identify if the page is running off of new saved code
+	echo '14'; //testing to identify if the page is running off of new saved code
 	echo '<br>';
 
 	//---$con=mysqli_connect("localhost","root","killerm111","abredb");
@@ -60,7 +60,7 @@
 	  printf("Connect failed: %s\n", mysqli_connect_error());
 	}
 
-	$array = array("2","7");
+	$array = array(2,7);
 
 	foreach($array as $value) {
   	print $value;
