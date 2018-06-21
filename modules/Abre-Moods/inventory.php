fbc40beaf50b64dbf94ddd193443e2168f65047b
sennem <marksenne000@gmail.com>
2018-06-21 10:39:02 -0400

    hfshs

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -51,7 +51,7 @@
 
 
 	echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>Record</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Here you see your mood history.</p></div>";
-	echo '18'; //testing to identify if the page is running off of new saved code
+	echo '19'; //testing to identify if the page is running off of new saved code
 	echo '<br>';
 
 	//---$con=mysqli_connect("localhost","root","killerm111","abredb");
@@ -96,7 +96,7 @@
 		echo $rowfeel . '|||';
 	}
 	echo '<br>';
-	if ($rowsfeeling[1]['Feeling']==1)
+	if ($rowsfeeling[2]['Feeling']==2)
 	{
 		echo 'can compare';
 	}else {
