1a244b64cfc03f507968185136d9b56ac93039a2
sennem <marksenne000@gmail.com>
2018-06-21 11:34:11 -0400

    emoji conversion works

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -80,42 +80,42 @@
 	}
 	foreach($rowsfeeling as $value)
 	{
-		 echo $value;
+		 echo '<br>';
 		 if($value==0)
 		 {
-			 	echo '<i class="em em-laughing " ></i>';
+			 	echo '<i class="em em-laughing EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==1)
 		 {
-			 	echo '<i class="em em-smiley " ></i>';
+			 	echo '<i class="em em-smiley EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==2)
 		 {
-			 	echo '<i class="em em-slightly_smiling_face " ></i>';
+			 	echo '<i class="em em-slightly_smiling_face EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==3)
 		 {
-			 	echo '<i class="em em-weary " ></i>';
+			 	echo '<i class="em em-weary EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==4)
 		 {
-			 	echo '<i class="em em-cry " ></i>';
+			 	echo '<i class="em em-cry EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==5)
 		 {
-			 	echo '<i class="em em-slightly_frowning_face " ></i>';
+			 	echo '<i class="em em-slightly_frowning_face EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==6)
 		 {
-			 	echo '<i class="em em-persevere " ></i>';
+			 	echo '<i class="em em-persevere EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==7)
 		 {
-			 	echo '<i class="em em-grimacing " ></i>';
+			 	echo '<i class="em em-grimacing EmojiSpacingLeft" ></i>';
 		 }
 		 if($value==8)
 		 {
-			 	echo '<i class="em em-expressionless " ></i>';
+			 	echo '<i class="em em-expressionless EmojiSpacingLeft" ></i>';
 		 }
 	}
 	echo '<br>';
