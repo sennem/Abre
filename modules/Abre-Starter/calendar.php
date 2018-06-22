da6961271921de0fbb842ccf04e1935c36ceeab0
sennem <marksenne000@gmail.com>
2018-06-22 14:31:30 -0400

    asdas

--- modules/Abre-Starter/calendar.php
+++ modules/Abre-Starter/calendar.php
@@ -8,7 +8,6 @@
 					<!--<iframe src="https://calendar.google.com/calendar/embed?src=marksenne000%40gmail.com&ctz=America%2FNew_York" style="padding:10px 30px 20px 30px" scrolling="no" height="400"></iframe>-->
 					<iframe src="https://calendar.google.com/calendar/embed?src=marksenne000%40gmail.com&ctz=America%2FNew_York" scrolling="no" height="200px"></iframe>
 					<!-- padding:10px 30px 20px 30px (top, right, bottom, left) -->
-					width="800" height="600" frameborder="0"
 			</div>
 		</div>
 	</div>
