0abcf25305632b9a5784a4e50dfed81e8d9bba12
sennem <marksenne000@gmail.com>
2018-06-22 13:57:35 -0400

    asdf

--- modules/Abre-Starter/calendar.php
+++ modules/Abre-Starter/calendar.php
@@ -1,1 +1,11 @@
-<iframe src="https://calendar.google.com/calendar/embed?src=marksenne000%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+
+<div class="col l4 s12">
+	<div class="row">
+		<div class="col s12">
+			<div class="mdl-shadow--2dp" style="background-color:#fff; padding:10px 30px 20px 30px">
+					<iframe src="https://calendar.google.com/calendar/embed?src=marksenne000%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+			</div>
+		</div>
+	</div>
+</div>
