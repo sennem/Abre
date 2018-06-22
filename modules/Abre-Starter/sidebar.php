6e0e303f8eb359435a831105797cb731cb2cd4ce
sennem <marksenne000@gmail.com>
2018-06-22 12:39:04 -0400

    asdf

--- modules/Abre-Starter/sidebar.php
+++ modules/Abre-Starter/sidebar.php
@@ -2,11 +2,16 @@
 <div class="col l4 s12">
 	<div class="row">
 		<div class="col s12">
-			<div class="mdl-shadow--2dp" style="background-color:#fff; padding:10px 30px 20px 30px"><h5 style="padding:5px;">My Friends</h5><table class="bordered"><tbody><tr class="classlink pointer" data="#students/2004INC/2"><td width="50px">
+			<div class="mdl-shadow--2dp" style="background-color:#fff; padding:10px 30px 20px 30px">
+				<h5 style="padding:5px;">My Friends</h5>
+				<table class="bordered"><tbody><tr class="classlink pointer" data="#students/2004INC/2"><td width="50px">
 				<div style="padding:5px; text-align:center; background-color:#2B2D4A; color:#fff; width:30px; height:30px; border-radius: 15px;">2</div>
-			</td><td>ALGEBRA I</td></tr><tr class="classlink pointer" data="#students/2004INC/1"><td width="50px"><div style="padding:5px; text-align:center; background-color:#2B2D4A; color:#fff; width:30px; height:30px; border-radius: 15px;">3</div></td>
+			</td><td>ALGEBRA I</td></tr><tr class="classlink pointer" data="#students/2004INC/1"><td width="50px">
+				<div style="padding:5px; text-align:center; background-color:#2B2D4A; color:#fff; width:30px; height:30px; border-radius: 15px;">3</div>
+			</td>
 				<td>ALGEBRA I</td></tr><tr class="classlink pointer" data="#students/2008INC/3">
-					<td width="50px"><div style="padding:5px; text-align:center; background-color:#2B2D4A; color:#fff; width:30px; height:30px; border-radius: 15px;">4</div></td><td>GEOMETRY S2</td></tr><tr class="classlink pointer" data="#students/2008INC/2"><td width="50px">
+					<td width="50px"><div style="padding:5px; text-align:center; background-color:#2B2D4A; color:#fff; width:30px; height:30px; border-radius: 15px;">4</div></td>
+					<td>GEOMETRY S2</td></tr><tr class="classlink pointer" data="#students/2008INC/2"><td width="50px">
 						<div style="padding:5px; text-align:center; background-color:#2B2D4A; color:#fff; width:30px; height:30px; border-radius: 15px;">5</div>
 					</td><td>GEOMETRY S2</td></tr>
 			<!--		<tr class="classlink pointer" data="#students/2008INC/1">
