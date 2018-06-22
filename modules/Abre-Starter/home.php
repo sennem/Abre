96cefc0d7f037606cd495b0c480e9651da058ec0
sennem <marksenne000@gmail.com>
2018-06-22 14:31:57 -0400

    asd

--- modules/Abre-Starter/home.php
+++ modules/Abre-Starter/home.php
@@ -68,7 +68,7 @@
 						<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect' href='#' style='color:<?php echo getSiteColor() ?>;'>Link to Homepage</a>
 					</div>
 				</div>
-				<?php require(dirname(__FILE__) . '/../../modules/Abre-Starter/calendar.php');?>
+				<?php require(dirname(__FILE__) . '/../../modules/Abre-Starter/calendar.php');?><!-- my pen is big -->
 			</div>
 			<br/>
 		</div>
