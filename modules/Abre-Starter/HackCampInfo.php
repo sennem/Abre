83fa117f9e6d1b650817fd1f6a6306b412cf0313
sennem <marksenne000@gmail.com>
2018-06-22 16:26:01 -0400

    final update for apathon

---
+++ modules/Abre-Starter/HackCampInfo.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+* Copyright (C) 2016-2017 Abre.io LLC
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the Affero General Public License version 3
+* as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the Affero General Public License
+* version 3 along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+*/
+
+//Required configuration files
+require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+//	<!-- hello world -->
+?>
+
+<div class='page_container mdl-shadow--4dp'>
+	<div class='page'>
+		<div class='row'>
+			<div style='padding:56px; text-align:center; width:100%; style="background-color:powderblue;'><span style='font-size: 22px; font-weight:700'>Weclome!</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Hack Camp Info Below</p></div>
+		</div>
+    <p align="center"> Hack Camp it Amazing </p>
+  </div>
+</div>
