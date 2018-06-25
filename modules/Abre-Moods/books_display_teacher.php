d927d71ca8711c712a63ee5981fc84de140e2de8
sennem <marksenne000@gmail.com>
2018-06-25 13:18:15 -0400

    wireframe moodmenu for teachers

---
+++ modules/Abre-Moods/books_display_teacher.php
@@ -0,0 +1,42 @@
+<?php
+
+	/*
+	* Copyright (C) 2016-2018 Abre.io Inc.
+	*
+	* This program is free software: you can redistribute it and/or modify
+    * it under the terms of the Affero General Public License version 3
+    * as published by the Free Software Foundation.
+	*
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+	*
+    * You should have received a copy of the Affero General Public License
+    * version 3 along with this program.  If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+    */
+
+		//--findme-- it got rid of thing
+
+
+    //Required configuration files
+	require(dirname(__FILE__) . '/../../configuration.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_verification.php');
+	require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
+	//require(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
+	require_once('functions.php');
+	require('permissions.php');
+
+	<div class="page_container">
+		<table style="width:100%">
+		<tr>
+			<th>Student</th>
+			<th>Typical Mood</th>
+		</tr>
+		<tr>
+			<td>sample name</td>
+			<td>average feeling</td>
+		</tr>
+		</table>
+	</div>
+?>
