f361c092d5ab10602c877eae833d03ab02086271
sennem <marksenne000@gmail.com>
2018-06-25 11:26:58 -0400

    asdf

--- modules/Abre-Moods/inventory.php
+++ modules/Abre-Moods/inventory.php
@@ -52,7 +52,7 @@
 	require_once('permissions.php');
 
 
-	echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>Reccord</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Here you see your mood history.</p></div>";
+	echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>Record</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Here you see your mood history.</p></div>";
 	//echo '26'; //testing to identify if the page is running off of new saved code
 	echo '<br>';
 
