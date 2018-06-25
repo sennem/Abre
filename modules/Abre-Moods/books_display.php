e215322482621a3a95c8fbdc674090a137eb60a8
sennem <marksenne000@gmail.com>
2018-06-25 10:57:45 -0400

    asdf

--- modules/Abre-Moods/books_display.php
+++ modules/Abre-Moods/books_display.php
@@ -45,7 +45,7 @@
 		<div class='row'>
 
 			<?php
-				echo '2';
+				echo '3';
 				echo "<div style='padding:30px; text-align:center; width:100%;'><span style='font-size: 22px; font-weight:700'>M0od Menu</span><br><p style='font-size:16px; margin:20px 0 0 0;'>Select an emoji that encapsulates your mood.</p></div>";
 			?>
 
