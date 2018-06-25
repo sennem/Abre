d45ba6a29a67a0a00160dfbf9a96313445cf4c6c
sennem <marksenne000@gmail.com>
2018-06-25 12:04:17 -0400

    asdf

--- modules/Abre-Event/routing.php
+++ modules/Abre-Event/routing.php
@@ -26,19 +26,9 @@
 				    $('#content_holder').hide();
 				    $('#loader').show();
 				    $('#titletext').text('Starter');
-				    document.title = 'Starter';
+				    document.title = 'Event';
 					$('#content_holder').load('modules/".basename(__DIR__)."/home.php', function() { init_page(); });
 					$( '#modal_holder' ).load( 'modules/".basename(__DIR__)."/modals.php' );
-			  },
-				'starter/elements': function()
-				{
-						$('#navigation_top').hide();
-						$('#content_holder').hide();
-						$('#loader').show();
-						$('#titletext').text('Starter');
-						document.title = 'Starter';
-					$('#content_holder').load('modules/".basename(__DIR__)."/elements.php', function() { init_page(); });
-					$( '#modal_holder' ).load( 'modules/".basename(__DIR__)."/modals.php' );
-				},";
+			  }";
 
 ?>
