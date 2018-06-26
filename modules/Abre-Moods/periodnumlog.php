df5a3660183513644c5b8bfce9b38313b78f0333
sennem <marksenne000@gmail.com>
2018-06-26 11:59:40 -0400

    got saving periodnum done

--- modules/Abre-Moods/periodnumlog.php
+++ modules/Abre-Moods/periodnumlog.php
@@ -9,14 +9,15 @@
   require_once(dirname(__FILE__) . '/../../core/abre_dbconnect.php');
   require_once(dirname(__FILE__) . '/../../core/abre_functions.php');
   // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-// sql to delete a record
-$sql = "DELETE FROM temp_hold_periodnum";
-$conn->query($sql);
-$sql = "INSERT INTO temp_hold_periodnum (periodnum) VALUES ('$periodnumget')";
-$conn->query($sql);
-$conn->close();
+  // sql to delete a record
+  $sql = "DELETE FROM temp_hold_periodnum";
+  $conn->query($sql);
+  $sql = "INSERT INTO temp_hold_periodnum (periodnum) VALUES ('$periodnumget')";
+  $conn->query($sql);
+  $conn->close();
   header("Location:http://localhost:8080/#books");
   exit;
+  //all works
 ?>
