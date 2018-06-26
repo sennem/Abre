fef3752fd844044ae77c4ff6a8d237b46e3eb330
sennem <marksenne000@gmail.com>
2018-06-26 13:45:23 -0400

    asdf

--- modules/Abre-Moods/books_display.php
+++ modules/Abre-Moods/books_display.php
@@ -352,7 +352,8 @@
 		echo $picurl . "picurl";
 		echo '<br>';
 ?>
-<img src="<?php echo $picurl; ?>" width="80" height="80" alt="Mark"> "<?php echo $fname; ?>"
+
+	<img src="<?php echo $picurl; ?>" width="80" height="80" alt="Mark"> <?php echo $fname . ' ' . $lname; ?>
 	<style>
 		img
 		{
